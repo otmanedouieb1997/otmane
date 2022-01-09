5d96341e0897013fd58c7ba8e0726143ad1cbc62
otmanedouieb1997 <otmane.douieb.1997@hotmail.com>
2022-01-09 11:36:25 +0100

    2

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
-clearConsole()
+const welcome = require('cli-welcome')
+const {name, version, description} = require('./package.json')
+
+welcome({
+    title : name,
+    tagLine : 'Start Learn NodeJs',
+    description : description,
+    version : version,
+    bgColor : '#00FF00',
+    color : '#000000',
+    clear : true
+})
+
+console.log(name)
+
+
+
 console.log('Hello')
